2fb601c88eb0e650bf9bfb97b778fc7a29ea5b86
JuanPabloGrossi <grossijuanpablo@gmail.com>
2026-09-16 20:31:49 -0300

    Agrega carga y particion de datos, comentarios en notebook de EDA

---
+++ src/data/load_data.py
@@ -0,0 +1,23 @@
+﻿import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from src.config import ID_COL, TARGET, RANDOM_STATE, TEST_SIZE
+
+
+def load_data(path):
+    df = pd.read_csv(path)
+    return df
+
+
+def split_data(df):
+    X = df.drop(columns=[TARGET, ID_COL])
+    y = df[TARGET]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=TEST_SIZE,
+        random_state=RANDOM_STATE,
+        stratify=y
+    )
+
+    return X_train, X_test, y_train, y_test
